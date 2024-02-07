1183e476a96a65b4f5ebaf30a8c23089016916fb
Ndutakimani <158273634+Ndutakimani@users.noreply.github.com>
2024-02-07 11:19:03 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a , b , c;
+printf("input first number :");
+scanf("%d" , &a);
+printf("input second number:");
+scanf("%d" , &b);
+c = a + b;
+printf("%d + %d = %d\n" , a,b,c);
+return 0;
+}
